de252b6acaff4670355d323284d3857d9a36234d
iamnaymur <nrtusher1@gmail.com>
2023-05-18 09:06:29 +0600

    addedToys

--- index.js
+++ index.js
@@ -8,12 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-
-
-
-const uri =
-  `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ac-wscs5t5-shard-00-00.7guimwk.mongodb.net:27017,ac-wscs5t5-shard-00-01.7guimwk.mongodb.net:27017,ac-wscs5t5-shard-00-02.7guimwk.mongodb.net:27017/?ssl=true&replicaSet=atlas-954gd2-shard-0&authSource=admin&retryWrites=true&w=majority`;
+const uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ac-wscs5t5-shard-00-00.7guimwk.mongodb.net:27017,ac-wscs5t5-shard-00-01.7guimwk.mongodb.net:27017,ac-wscs5t5-shard-00-02.7guimwk.mongodb.net:27017/?ssl=true&replicaSet=atlas-954gd2-shard-0&authSource=admin&retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -28,6 +23,21 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const collectionToyDB = client.db("toyVerseDB").collection("categoriesToy");
+    const addToyToDB = client.db("toyVerseDB").collection("addToy");
+
+    app.get("/toyCategories", async (req, res) => {
+      const result = await collectionToyDB.find().toArray();
+      res.send(result);
+    });
+
+    app.post('/addedToys', async (req, res) => {
+      const added = req.body
+      // console.log(added)
+      const result= await addToyToDB.insertOne(added)
+      res.send(result);
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -39,7 +49,6 @@ async function run() {
   }
 }
 run().catch(console.dir);
-
 
 app.get("/", (req, res) => {
   res.send("toy-verse is running");
