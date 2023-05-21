fd791ce6bdbec07359c8cc69af307e4ca7b009ae
iamnaymur <nrtusher1@gmail.com>
2023-05-21 09:45:45 +0600

    client.connect comment

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    client.connect();
+    // client.connect();
 
     const addToyToDB = client.db("toyVerseDB").collection("addToy");
 
